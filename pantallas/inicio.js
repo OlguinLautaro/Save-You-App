58f5f4cb2cdd979a550d8e45bd4d3e0607a18903
Joaquin <joaquineaceituno@gmail.com>
2023-12-07 15:29:38 -0300

    se agrega el boton para crear contacto

--- pantallas/inicio.js
+++ pantallas/inicio.js
@@ -1,10 +1,39 @@
 import react from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation} from '@react-navigation/native'; 
 
 export default function Inicio() {
+const navigation = useNavigation();
+    const agregar = () => {
+        navigation.navigate("Contacto")
+    }
     return (
-        <View>
-            <Text>Boton</Text>
+        <View style={styles.container}>
+            
+            <TouchableOpacity style={styles.boton} onPress={agregar}>
+                <Text style={styles.textoBoton}>Agregar contacto</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+      },
+      boton: {
+        backgroundColor: 'blue',
+        paddingVertical: 12,
+        paddingHorizontal: 15,
+        borderRadius: 7,
+        marginTop: 20
+      },
+      textoBoton: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+      },
+});
